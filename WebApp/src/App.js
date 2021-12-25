b07ec8dbfcc926dbad5d3b405dce3b21dd5e264e
Arya Soni <aryasoni98@gmail.com>
2021-12-25 18:26:35 +0530

    Bug Fix

---
+++ WebApp/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Page1 from "./Component/Page1";
+
+function App() {
+  return (
+    <div>
+      <Page1 />
+    </div>
+  );
+}
+
+export default App;
